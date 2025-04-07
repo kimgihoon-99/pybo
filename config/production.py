65c918112071d6b2abd4b2fe57965f61d5835add
김기훈 <sjcjaj@gmail.com>
2025-04-07 16:17:34 +0900

    SQL 추가

--- config/production.py
+++ config/production.py
@@ -1,7 +1,14 @@
 from logging.config import dictConfig
 from config.default import *
+from dotenv import load_dotenv
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///{}'.format(os.path.join(BASE_DIR, 'pybo.db'))
+load_dotenv(os.path.join(BASE_DIR, '.env'))
+
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
+    user=os.getenv('DB_USER'),
+    pw=os.getenv('DB_PASSWORD'),
+    url=os.getenv('DB_HOST'),
+    db=os.getenv('DB_NAME'))
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SECRET_KEY = b'd\xb8(\xc0\xbf\xef\xeao\xf8!\xe2\xb8\x81C\xda\xb1'
 
